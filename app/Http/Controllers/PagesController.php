6bf2a220a554fc17858cdd8645e79f2888a5450b
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-07 01:47:57 +0900

    setting controller@gethome

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function getHome(){
+        return view('home');
+    }
+
+
 }
