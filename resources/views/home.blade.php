0d600828363c90283e1e013b3514678cd19e9c23
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-09 22:59:14 +0900

    view file updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,21 @@
-<H1>php</h1>
+<html>
+  <head>
+    <title>This is POPBOARD</title>
+  </head?>
+  <body>
+    <b>Pop Board</b>
+    <form method="POST">
+      @csrf
+      <label>NickName</label>
+      <input type="text" name="nickname"/>
+      <label>Comment</label>
+      <input type="text" name="comment"/>
+      <input type="submit" value="OK"/>
+    </form>
+    
+    <hr>
+    @foreach ($posts as $post)
+      <p><b>{{$post["nickname"]}} </b>{{$post["comment"]}}</P>
+    @endforeach
+  </body>
+</html>
