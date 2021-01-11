39fba0a39cc768c1abd95d1f7ea65822e3066881
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-11 15:53:50 +0900

    change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,9 +26,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-
-        if (\App::environment('production')){
-            \URL::forceScheme('https');
-        }
+        
     }
 }
