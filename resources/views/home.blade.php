a7ca7cefd631b6a728e7abe04ca200c27e767a42
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-12 23:03:48 +0900

    html, css initial setting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,17 @@
+<!DOCTYPE html>
+
 <html>
   <head>
+    <meta charset=="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
     <title>This is POPBOARD</title>
+    <link rel="stylesheet" href="/css/style.css">
   </head?>
   <body>
     <b>Pop Board</b>
     <form method="POST">
       @csrf
-      <label>NickName</label>
+      <label class='name'>NickName</label>
       <input type="text" name="nickname"/>
       <label>Comment</label>
       <input type="text" name="comment"/>
