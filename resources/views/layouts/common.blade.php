4c4ecdc2da8f903e9fcfaf0f08793a02abe936fa
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-17 17:41:35 +0900

    layouts file made

---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset=="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title>Design comment</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    @include('parts.header')
+    @yield('content')
+  </body>
+</html>
