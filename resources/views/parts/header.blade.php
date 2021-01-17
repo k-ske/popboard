86dbca67a7822d9c2d9fd5d26319b323c1bb80ff
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-17 17:41:45 +0900

    header file made

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,10 @@
+<header class='top-page-header'>
+  <div class='top-bar-contents'>
+    <form class="search-form" action="#">
+      <input class='input-box' placeholder='キーワードから探す'>
+      <button class="search-button">
+        <%= image_tag "search.png", class:"search-icon" %>
+      </button>
+    </form>
+  </div>
+</header>
