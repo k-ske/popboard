55fb2dbdcab2bb2a96e7f02a16ca419081c3c612
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-17 15:14:36 +0900

    image to comment attached

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,21 +6,33 @@
     <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
     <title>This is POPBOARD</title>
     <link rel="stylesheet" href="/css/style.css">
-  </head?>
+  </head>
   <body>
-    <b>Pop Board</b>
-    <form method="POST">
-      @csrf
-      <label class='name'>NickName</label>
-      <input type="text" name="nickname"/>
-      <label>Comment</label>
-      <input type="text" name="comment"/>
-      <input type="submit" value="OK"/>
-    </form>
+    <b>ろうけつ染め展（2020.12/11~14)</b>
+    <div class="field">
+      <div class="image-field">
+        <div class="image">
+          <img src="image/image3.jpg" alt="作品" class="work1">
+          <p>T-shirt Collection</p>
+        </div>
+      </div>
+      <div class="comment-field">
+        <form method="POST">
+          @csrf
+          <label class='name'>NickName</label>
+          <input type="text" name="nickname"/>
+          <label>Comment</label>
+          <input type="text" name="comment"/>
+          <input type="submit" value="OK"/>
+          
+          <hr>
+          @foreach ($posts as $post)
+            <p><b>{{$post["nickname"]}} </b>{{$post["comment"]}}</P>
+          @endforeach
+        </form>
+      </div>
+    </div>
     
-    <hr>
-    @foreach ($posts as $post)
-      <p><b>{{$post["nickname"]}} </b>{{$post["comment"]}}</P>
-    @endforeach
+    
   </body>
 </html>
