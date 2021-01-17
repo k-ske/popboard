ffce7d99345b22382acdee116b7f598bb22281f1
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2021-01-18 01:26:58 +0900

    use s3 url

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,6 +1,6 @@
 <header class='top-page-header'>
   <div class='top-bar-contents'>
     <p class="site-name">染色作家のデザイン実況室</p>
-    <img src="image/image3.png" class="top-bar-picture">
+    <img src="https://k20k20sk.s3-ap-northeast-1.amazonaws.com/works_image/image3.PNG" class="top-bar-picture">
   </div>
 </header>
